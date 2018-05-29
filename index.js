5d54253c2807fbda2e9792bcf77cc233aa385b4e
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-28 17:31:16 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import isType from "@unction/istype"
-
-export default function isNil (value: mixed): boolean {
-  return isType("undefined")(value) || isType("null")(value)
+import isType from "@unction/istype";
+export default function isNil(value) {
+  return isType("undefined")(value) || isType("null")(value);
 }
